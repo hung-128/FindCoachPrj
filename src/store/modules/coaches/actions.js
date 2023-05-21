d1ea0ce43d764b97cff84c7ca36d251bf377724c
hungpham128 <hungdbi1281999@gmail.com>
2023-05-22 02:15:07 +0700

    register coach

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+    registerCoach(context, payload) {
+        const newCoach = {
+            id: 'c3',
+            firstName: payload.data.first,
+            lastName: payload.data.last,
+            description: payload.data.description,
+            areas: payload.data.areas,
+            hourlyRate: payload.data.rate,
+        };
+        context.commit('registerCoach', { data: newCoach })
+    }
+};
